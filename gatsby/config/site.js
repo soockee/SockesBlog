469d71b5c789f9ea9e9b9b6ee3d2c0aeae83fef1
Simon Stockhause <simon.stockhause@igd.fraunhofer.de>
2020-06-29 17:48:03 +0200

    some metadata

--- gatsby/config/site.js
+++ gatsby/config/site.js
@@ -10,9 +10,9 @@ module.exports = {
   banner: 'static/logo/banner.png',
   // JSONLD / Manifest
   favicon: 'static/logo/favicon.png', // Used for manifest favicon generation
-  shortName: 'GatsbyTut', // shortname for manifest. MUST be shorter than 12 characters
-  author: 'Justin', // Author for schemaORGJSONLD
+  shortName: 'Stocki', // shortname for manifest. MUST be shorter than 12 characters
+  author: 'Simon', // Author for schemaORGJSONLD
   themeColor: '#3e7bf2',
   backgroundColor: '#d3e0ff',
-  twitter: '@justinformentin', // Twitter Username
+  twitter: '', // Twitter Username
 };
