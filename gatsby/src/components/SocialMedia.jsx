37311e28ae4e1df7c8d447093f2188e1dd970b5b
Soockee <gamingsockee@gmail.com>
2020-09-01 14:02:40 +0200

    linkedin button added

--- gatsby/src/components/SocialMedia.jsx
+++ gatsby/src/components/SocialMedia.jsx
@@ -10,10 +10,11 @@ const Wrapper = styled.div`
   justify-content: center;
   align-items: start;
 `;
-const SocialMedia = ({ instagramURL, githubURL, socialIconStyle }) => {
+const SocialMedia = ({ instagramURL, githubURL,linkedinURL, socialIconStyle }) => {
   var socialIconStyleAssign = Object.assign({}, socialIconStyle.display);
   const [hoverInsta, setHoverInsta] = useState(false);
   const [hoverGithub, setHoverGithub] = useState(false);
+  const [hoverLinkedin, setHoverLinkedin] = useState(false);
   return (
     <Wrapper>
       <SocialIcon
@@ -42,18 +43,33 @@ const SocialMedia = ({ instagramURL, githubURL, socialIconStyle }) => {
           !hoverGithub ? theme.colors.white.base : theme.colors.black.base
         }
       />
+      <SocialIcon
+        url={linkedinURL}
+        style={socialIconStyleAssign}
+        onMouseOver={() => {
+          setHoverLinkedin(true);
+        }}
+        onMouseOut={() => {
+          setHoverLinkedin(false);
+        }}
+        bgColor={
+          !hoverLinkedin ? theme.colors.white.base : theme.colors.black.base
+        }
+      />
     </Wrapper>
   );
 };
 
 SocialMedia.propTypes = {
   instagramURL: PropTypes.string,
+  linkedinURL: PropTypes.string,
   githubURL: PropTypes.string,
   stackoverflowURL: PropTypes.string,
   socialIconStyle: PropTypes.string,
 };
 SocialMedia.defaultProps = {
   instagramURL: 'https://www.instagram.com/sockeod/',
+  linkedinURL: 'https://www.linkedin.com/in/simon-stockhause-58982b1b5',
   githubURL: 'https://github.com/Soockee',
   stackoverflowURL: 'https://stackoverflow.com/users/7383590/socke',
   socialIconStyle: '{(display: flex)}',
