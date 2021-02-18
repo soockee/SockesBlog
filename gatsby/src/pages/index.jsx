8292fcc41e7251446fb833ff44017752ba54d91f
Soockee <gamingsockee@gmail.com>
2021-02-18 19:20:01 +0100

    changed design, wave header, banner, new landing page background image

--- gatsby/src/pages/index.jsx
+++ gatsby/src/pages/index.jsx
@@ -3,9 +3,42 @@ import { graphql } from 'gatsby';
 import PropTypes from 'prop-types';
 import Helmet from 'react-helmet';
 import styled from '@emotion/styled';
+import Img from 'gatsby-image';
 import { Header, PostList } from 'components';
 import { Layout } from 'layouts';
 
+
+const Wrapper = styled.article`
+  margin-bottom: 2rem;
+  position: relative;
+  z-index: 100;
+  border-radius: ${props => props.theme.borderRadius.default};
+  box-shadow: ${props => props.theme.shadow.feature.small.default};
+  transition: ${props => props.theme.transitions.boom.transition};
+  height: 17rem;
+  flex-basis: calc(99.9% * 1 / 3 - 2.5rem);
+  max-width: calc(99.9% * 1 / 3 - 2.5rem);
+  width: calc(99.9% * 1 / 3 - 2.5rem);
+
+  &:hover {
+    box-shadow: ${props => props.theme.shadow.feature.small.hover};
+    transform: scale(1.04);
+  }
+
+  @media (max-width: 1000px) {
+    flex-basis: calc(99.9% * 1 / 2 - 1rem);
+    max-width: calc(99.9% * 1 / 2 - 1rem);
+    width: calc(99.9% * 1 / 2 - 1rem);
+    height: 18rem;
+  }
+
+  @media (max-width: 700px) {
+    flex-basis: 100%;
+    max-width: 100%;
+    width: 100%;
+    height: 15rem;
+  }
+`;
 const PostWrapper = styled.div`
   display: flex;
   flex-direction: row;
@@ -19,13 +52,13 @@ const PostWrapper = styled.div`
     margin: 4rem 1rem 1rem 1rem;
   }
 `;
-
 const Index = ({ data }) => {
   const { edges } = data.allMarkdownRemark;
   return (
     <Layout>
       <Helmet title={'Sockes Blog'} />
-      <Header title="Sockes Blog"></Header>
+      <Header title="Sockes Blog" cover={data.file.childImageSharp.fluid}></Header>
+
       <PostWrapper>
         {edges.map(({ node }) => {
           const { id, excerpt, frontmatter } = node;
@@ -97,6 +130,16 @@ export const query = graphql`
               }
             }
           }
+        }
+      }
+    }
+    file(relativePath: { eq: "main_background.png" }) {
+      childImageSharp {
+        fluid(
+          maxWidth: 1920
+          quality: 90
+        ) {
+          ...GatsbyImageSharpFluid_withWebp_tracedSVG
         }
       }
     }
